f7f2405394267d27b272b3721aa912aceeecb07e
Matis Rodier <22203556@dinfo.iut-velizy.uvsq.fr>
2023-09-12 13:48:33 +0200

    ajout de la classe et des fonctions necessaire pour demarrer

---
+++ Demineur.py
@@ -0,0 +1,21 @@
+class Demineur:
+    def __init__(self, tailleCadr, nbBombes):
+        pass
+
+    def generationCadr(self):
+        pass
+
+    def mettreDrapeau(self):
+        pass
+
+    def decouvrirCase(self):
+        pass
+
+    def gameOver(self):
+        pass
+
+    def gameWon(self):
+        pass
+
+    def __str__(self):
+        pass
