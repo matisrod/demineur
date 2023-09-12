dac5aa28c623056c178cc4dba3626a7058328b3e
Thomas GODINEAU <thomasgodineau2004@gmail.com>
2023-09-12 13:57:44 +0200

    Partie de la doc de Thomas

--- Demineur.py
+++ Demineur.py
@@ -3,12 +3,25 @@ class Demineur:
         pass
 
     def generationCadr(self):
+        """
+        Génère le Cadrillage en fonction du nombre de bombe
+        """
         pass
 
-    def mettreDrapeau(self):
+    def drapeau(self):
+        """
+        Met un drapeau sur la case ou l'enlève s'il y en a déjà un
+        """
         pass
 
     def decouvrirCase(self):
+        """
+        Découvre la case choisit :
+
+        - Si c'est une bombe, appelle la fonction gameOver
+        - Sinon Si toutes les cases non-bombes sont découvertes, appelle la fonction gameWon
+        - Sinon découvre la case
+        """
         pass
 
     def gameOver(self):
